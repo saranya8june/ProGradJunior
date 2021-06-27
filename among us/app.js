e20249d39db4fb1aedb373db0889d732bc49a6a1
saranya8june <86546679+saranya8june@users.noreply.github.com>
2021-06-27 11:45:14 +0530

    Added Among Us

---
+++ among us/app.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("button");
+
+const red = document.getElementById("red");
+const blue = document.getElementById("blue");
+const yellow = document.getElementById("yellow");
+
+const redImage = document.getElementById("red-image");
+const yellowImage = document.getElementById("yellow-image");
+const blueImage = document.getElementById("blue-image");button.onclick = () => {
+    if (red.checked) {
+    redImage.classList.remove("hidden");
+    redImage.classList.add("eject");
+    }
+    if (blue.checked) {
+    blueImage.classList.remove("hidden");
+    blueImage.classList.add("eject");
+    }
+    if (yellow.checked) {
+    yellowImage.classList.remove("hidden");
+    yellowImage.classList.add("eject");
+    }
+    }
